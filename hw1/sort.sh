90b3f7a859d6a0e71a378794cf671e3a7b52ecbf
Pepper Chicken <52809000+pepper-chicken@users.noreply.github.com>
2020-04-12 22:20:41 -0700

    Update sort.sh

--- hw1/sort.sh
+++ hw1/sort.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N mergesort
 #$ -q class 
-#$ -pe openmp 16
+#$ -pe openmp 8
 
 # Module load intel compiler 
 module load intel-parallel-studio-xe/15.0.3
@@ -10,7 +10,7 @@ module load intel-parallel-studio-xe/15.0.3
 # The OMP_NUM_THREADS variable is automaticaly set to the
 # core count of our job.   No need to set it again, but here
 # it is.
-export OMP_NUM_THREADS=16
+export OMP_NUM_THREADS=8
 
 # Run quicksort on 10 million elements
 ./mergesort2 10000000
