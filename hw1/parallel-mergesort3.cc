fb7945cf53546eb62b3dbf74cbf5b071a716c5d1
Pepper Chicken <52809000+pepper-chicken@users.noreply.github.com>
2020-04-19 22:52:47 -0700

    Update parallel-mergesort3.cc

--- hw1/parallel-mergesort3.cc
+++ hw1/parallel-mergesort3.cc
@@ -108,10 +108,10 @@ keytype *Pmerge(keytype* A, keytype* B, int a, int b) {
          b2 = 0;
       }
       keytype *tmp1, *tmp2;
-      #pragma omp task shared(tmp1)
+      #pragma omp task
       tmp1 = Pmerge(A, B, a1, b1);
       
-      #pragma omp task shared(tmp2)
+      #pragma omp task
       tmp2 = Pmerge(A+a1, B+b1, a2, b2);
 
       #pragma omp taskwait
