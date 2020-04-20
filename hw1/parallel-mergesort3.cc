76d29953dc9518b72696b4cfeea3a0b7d556672c
Pepper Chicken <52809000+pepper-chicken@users.noreply.github.com>
2020-04-19 21:41:20 -0700

    Update parallel-mergesort3.cc

--- hw1/parallel-mergesort3.cc
+++ hw1/parallel-mergesort3.cc
@@ -14,7 +14,7 @@
 #include <vector>
 #include <cstring>
 
-#define PAR_TH 1000
+#define PAR_TH 4000
 
 int binarySearch(keytype* A, int N, int target) 
 { 
@@ -69,61 +69,6 @@ keytype* merge(keytype* A, keytype* B, int a, int b) {
 
 } // end of merge()
 
-void merge2(keytype* A1, keytype* A2, int N1, int N2, keytype* tmp) {
-   int i = 0;
-   int j = 0;
-   int ti = 0;
-
-   while (i < N1 && j < N2) {
-      if (A1[i] < A2[j]) {
-         tmp[ti] = A1[i];
-         ti++; 
-         i++;
-      } 
-      else {
-         tmp[ti] = A2[j];
-         ti++; 
-         j++;
-      }
-   }
-   while (i < N1) { /* finish up lower half */
-      tmp[ti] = A1[i];
-      ti++; 
-      i++;
-   }
-   while (j < N2) { /* finish up upper half */
-      tmp[ti] = A2[j];
-      ti++; 
-      j++;
-   }
-   //memcpy(dest, tmp, (N1 + N2)*sizeof(keytype));
-
-} // end of merge()
-
-
-
-// void Pmerge2(keytype* A, keytype* B, int a, int b, keytype* tmp){
-//    if (a+b < PAR_TH){
-//       merge2(A, B, a, b, tmp);
-//    }
-//    else{
-
-//       int midA = a / 2;
-//       int midB = binarySearch(B, b, A[midA]);
-//       int a1 = midA, a2 = a - midA, b1 = midB, b2 = b - midB;
-//       if (midB == b - 1 && B[midB] <= A[midA]){
-//          b1 = b;
-//          b2 = 0;
-//       }
-//       #pragma omp task
-//       Pmerge2(A, B, a1, b1, tmp);
-//       #pragma omp task
-//       Pmerge2(A+a1, B+b1, a2, b2, tmp+a1+b1);
-//       #pragma omp taskwait
-//       //memcpy(A, tmp, sizeof(keytype)*(a+b));
-//    }
-
-// }
 
 void mergeSort(keytype* A, int N)
 {
@@ -141,8 +86,8 @@ void mergeSort(keytype* A, int N)
       mergeSort(A+(N/2), N-(N/2));
    }
 
-    keytype* tmp = merge(A, A+(N/2), N/2, N-(N/2));
-    memcpy(A, tmp, sizeof(keytype) * N);
+   keytype* tmp = merge(A, A+(N/2), N/2, N-(N/2));
+   memcpy(A, tmp, sizeof(keytype) * N);
 }
 
 
