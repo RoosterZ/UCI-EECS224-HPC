3d8ede37d3ccf8f52d7987f78afd3e1e4e286047
Pepper Chicken <52809000+pepper-chicken@users.noreply.github.com>
2020-05-06 08:13:45 -0700

    Update par.sh

--- hw2/par.sh
+++ hw2/par.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N Mandelbrot_Joe
 #$ -q class
-#$ -pe mpi 8
+#$ -pe mpi 16
 #$ -R y
 
 # Grid Engine Notes:
@@ -17,4 +17,4 @@ module load boost/1.57.0
 module load mpich-3.0.4/gcc-4.8.3
 
 # Run the program 
-mpirun -np 12 -ppn 12  ./mandelbrot_joe 1000 1000 8
+mpirun -np 16 -ppn 8  ./mandelbrot_joe 1000 1000 8
