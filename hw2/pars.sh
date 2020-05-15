d55a000c49122f9c2e48b9a05f7c98103a8fcab3
Pepper Chicken <52809000+pepper-chicken@users.noreply.github.com>
2020-05-15 15:46:17 -0700

    Update pars.sh

--- hw2/pars.sh
+++ hw2/pars.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #$ -N Mandelbrot_Susie
-#$ -q pub8i
-#$ -pe mpi 8
+#$ -q pub8iq
+#$ -pe mpi 32
 #$ -R y
 
 # Grid Engine Notes:
@@ -16,14 +16,14 @@ module load boost/1.57.0
 # Module load OpenMPI
 module load mpich-3.0.4/gcc-4.8.3
 
-trial = 4
-if_render = 0
-start_sz = 256
-end_sz = 16384
+trial=4
+if_render=0
+start_sz=256
+end_sz=16384
 
 # Run the program 
 mpirun -np 2    ./mandelbrot_susie $start_sz $end_sz $trial $if_render
-# mpirun -np 4    ./mandelbrot_susie $(start_sz) $(end_sz) $(trial) $(if_render)
-# mpirun -np 8    ./mandelbrot_susie $(start_sz) $(end_sz) $(trial) $(if_render)
-# mpirun -np 16   ./mandelbrot_susie $(start_sz) $(end_sz) $(trial) $(if_render)
-# mpirun -np 32   ./mandelbrot_susie $(start_sz) $(end_sz) $(trial) $(if_render)
+mpirun -np 4    ./mandelbrot_susie $start_sz $end_sz $trial $if_render
+mpirun -np 8    ./mandelbrot_susie $start_sz $end_sz $trial $if_render
+mpirun -np 16   ./mandelbrot_susie $start_sz $end_sz $trial $if_render
+mpirun -np 32   ./mandelbrot_susie $start_sz $end_sz $trial $if_render
