4cced5fce3d61682fb7de649f690d757a129cd28
Pepper Chicken <52809000+pepper-chicken@users.noreply.github.com>
2020-05-31 16:22:39 -0700

    Create transpose.sh

---
+++ hw3/ref/transpose.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#$ -N REDUCE
+#$ -q gpu
+
+# Notes: 
+# The GPU node (compute-1-14) has 4 Tesla M2090 GPU cards. The node also
+# has 24 Intel cores.
+#
+# To see how many GPU cards compute-1-14 has avaialble for a job:
+# qhost -F gpu -h compute-1-14
+#
+# For details, see: http://hpc.oit.uci.edu/gpu
+#
+
+# Module load Cuda Compilers and GCC
+module load  cuda/8.0
+module load  gcc/4.9.0
+
+# Runs a bunch of standard command-line
+# utilities, just as an example:
+
+./ref
+
+# eof
